8bf1201511dcb59216c16291b012992344aa6c60
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-08-02 15:06:10 +0300

    Add project files.

---
+++ Inspimo_Ajax/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inspimo_Ajax.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SAADET\\SQLEXPRESS01;initial Catalog=AjaxDb;integrated Security=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
